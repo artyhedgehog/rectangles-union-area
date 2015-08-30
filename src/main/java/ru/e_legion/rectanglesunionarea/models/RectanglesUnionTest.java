0bc515d54eba8e47a121bc5450620a075c4734c1
Artem Kolomycev <artyhedgehog@gmail.com>
2015-08-31 00:27:23 +0300

    Added stress testing. The test passes.

--- src/main/java/ru/e_legion/rectanglesunionarea/models/RectanglesUnionTest.java
+++ src/main/java/ru/e_legion/rectanglesunionarea/models/RectanglesUnionTest.java
@@ -67,4 +67,23 @@ public class RectanglesUnionTest {
              .add(new Rectangle(1, 1, 4, 3));
         assertEquals(17, union.area());
     }
+
+    @Test
+    public void testHighload() throws Exception {
+        final Runtime runtime = Runtime.getRuntime();
+        RectanglesUnion union = new RectanglesUnion();
+        long usedMemoryMB;
+        int x1, y1, x2, y2;
+        for (int i = 0; i < 100; ++i) {
+            x1 = i * 100 - 10_000;
+            y1 = -10_000;
+            x2 = (i + 1) * 100;
+            y2 = 10_000;
+            union.add(new Rectangle(x1, y1, x2, y2));
+            usedMemoryMB = (runtime.totalMemory() - runtime.freeMemory())
+                    / 1024 / 1024;
+            assertTrue(usedMemoryMB < 16);
+        }
+        assertEquals(400_000_000, union.area());
+    }
 }
